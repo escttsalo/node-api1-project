00c6bd80f7fafa63a76ea48cf24631cb5499e4bb
escttsalo <escttsalo@gmail.com>
2021-07-10 02:14:32 -0600

    get users endpoint

--- api/server.js
+++ api/server.js
@@ -24,4 +24,16 @@ server.post('/api/users', (req, res) => {
     }
 });
 
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The users information could not be retrieved" })
+        }) 
+})
+
+
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
