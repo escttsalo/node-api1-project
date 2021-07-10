2ddc605331fbe6308cd2f138723f7333d7b8ccda
escttsalo <escttsalo@gmail.com>
2021-07-10 03:32:14 -0600

    put endpoint

--- api/server.js
+++ api/server.js
@@ -13,7 +13,6 @@ server.post('/api/users', (req, res) => {
 
         User.insert({name, bio})
             .then(user => {
-                console.log(user)
                 res.status(201).json(user)
             })
             .catch(err => {
@@ -68,4 +67,24 @@ server.delete('/api/users/:id', async (req, res) => {
    }
 })
 
+
+server.put('/api/users/:id', async (req, res) => {
+    const { id } = req.params
+    const { name, bio } = req.body
+    if (!name || !bio){
+        res.status(400).json({ message: "Please provide name and bio for the user" })
+    } else {
+        try {
+            const updatedUser = await User.update(id, {name, bio})
+            console.log(updatedUser)
+            if (!updatedUser) {
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            } else {
+                res.status(202).json(updatedUser)
+            }
+        } catch (err) {
+            res.status(500).json({ message: "The user information could not be modified" })
+        }
+    }
+});
 module.exports = server; // EXPORT YOUR SERVER instead of {}
